6ea2103e6724931126513288f5c4735b11854643
Ned Zimmerman <ned@bight.dev>
2022-05-21 17:12:35 -0400

    fix: rename config param to match Markdoc conventions

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const Markdoc = require("@markdoc/markdoc");
 
 module.exports = {
     initArguments: {},
-    configFunction: function (eleventyConfig, options = {}) {
+    configFunction: function (eleventyConfig, config = {}) {
         eleventyConfig.addTemplateFormats("markdoc.js");
         eleventyConfig.addExtension("markdoc.js", {
             compile: async (inputContent) => {
                 const ast = Markdoc.parse(inputContent)
-                const content = Markdoc.transform(ast, options);
+                const content = Markdoc.transform(ast, config);
                 const html = Markdoc.renderers.html(content);
         
                 return async () => {
