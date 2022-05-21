b1df1621839bd0e721a2bf39afc621203e04a973
Ned Zimmerman <ned@bight.dev>
2022-05-21 16:56:18 -0400

    feat: basic custom template format implementation for Markdoc

---
+++ index.js
@@ -0,0 +1,19 @@
+const Markdoc = requre("@markdoc/markdoc");
+
+module.exports = {
+    initArguments: {},
+    configFunction: function (eleventyConfig, options = {}) {
+        eleventyConfig.addTemplateFormats("markdoc.js");
+        eleventyConfig.addExtension("markdoc.js", {
+            compile: async (inputContent) => {
+                const ast = Markdoc.parse(inputContent)
+                const content = Markdoc.transform(ast, options);
+                const html = Markdoc.renderers.html(content);
+        
+                return async () => {
+                    return html;
+                };
+            }
+        });
+    }
+};
